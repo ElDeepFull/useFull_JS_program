d1e0bd2225dcf982b1102ef98aec3535bfc11da4
elDeepFull <85937379+ElDeepFull@users.noreply.github.com>
2023-08-30 15:09:54 +0200

    Add new program

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// PROGRAM 1
+
 const sumArray = function (array) {
   let sum = 0;
   array.forEach(function (number) {
@@ -8,3 +10,33 @@ const sumArray = function (array) {
 
 sumArray([2, 2, 2]);
 console.log(sumArray([2, 10, 22]));
+
+// PROGRAMM 2
+
+const contacts = [
+  "Chris:2232322",
+  "Sarah:3453456",
+  "Bill:7654322",
+  "Mary:9998769",
+  "Dianne:9384975",
+];
+
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const bouton = document.querySelector("button");
+
+bouton.addEventListener("click", function () {
+  let searchName = input.value;
+  input.value = "";
+  input.focus();
+  for (let i = 0; i < contacts.length; i++) {
+    let splitContact = contacts[i].split(":");
+    if (splitContact[0] === searchName) {
+      para.textContent =
+        splitContact[0] + "'s number is " + splitContact[1] + ".";
+      break;
+    } else {
+      para.textContent = "Contact not found.";
+    }
+  }
+});
